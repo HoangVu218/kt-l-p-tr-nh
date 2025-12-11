fd32004e6741de85acdbe8a00a78c3207adbfba7
HoangVu218 <nguyenhoangvu2182006@gmail.com>
2025-12-11 14:20:02 +0700

    Add files via upload

---
+++ bai 3/bai 3.3.py
@@ -0,0 +1,11 @@
+print("Nguyen Hoang Vu")
+print("245752021610136")
+print("--------------------")
+# Sửa lỗi: In biến a bên trong hàm
+def say_hello():
+    a = "hello" 
+    print(a)     # In biến a bên trong hàm
+    print(a)     # In biến a bên trong hàm
+
+# Gọi hàm để chạy code bên trong
+say_hello()
